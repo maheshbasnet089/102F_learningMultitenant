716ab801b10737926daef8890b711fde5f67ea3c
maheshbasnet089 <basnetmanish089@gmail.com>
2023-10-15 20:33:22 +0545

    learn socket

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ const server = app.listen(3000,()=>{
 const io = new Server(server)
 
 io.on("connection",(socket)=>{
+
+        console.log("User connected")
     socket.on("register",async (data)=>{
         const {username,password,email} = data
         await users.create({
@@ -41,8 +43,11 @@ io.on("connection",(socket)=>{
             email
         })
         socket.emit("response",{status : 200,message : 'Registered '})
+        
     })
-
+    socket.on("disconnect",()=>{
+        console.log("User disconnected")
+    })
 
     // socket.on("hello",(data)=>{
     //     console.log(data)
