4df8091c87bff8fc2b46e5d4cfcafedb571db015
maheshbasnet089 <basnetmanish089@gmail.com>
2023-10-15 20:23:34 +0545

    learn socket

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const app = express()
 
 const userRoute  = require("./route/userRoute")
 const organizationRoute  = require("./route/organizationRoute")
+const { Server } = require("socket.io")
+const { users } = require("./model/index")
 app.use(require('cookie-parser')())
 
 // require database 
 require("./model/index")
+
+app.set("view engine","ejs")
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
 app.get("/",(req,res)=>{
-    res.send("I am alive")
+    res.render("home")
 })
 
 app.use("",userRoute)
@@ -22,6 +26,26 @@ app.use("",organizationRoute)
 
 
 
-app.listen(3000,()=>{
+const server = app.listen(3000,()=>{
     console.log("Nodejs has started at port 3000")
+})
+
+const io = new Server(server)
+
+io.on("connection",(socket)=>{
+    socket.on("register",async (data)=>{
+        const {username,password,email} = data
+        await users.create({
+            username,
+            password,
+            email
+        })
+        socket.emit("response",{status : 200,message : 'Registered '})
+    })
+
+
+    // socket.on("hello",(data)=>{
+    //     console.log(data)
+    //     socket.emit("response","Your data was received in backend")
+    // })
 })
